de96c3e2d05134c5077a98b0887e912f454a2711
Rafhaan Shah <RafhaanShah@users.noreply.github.com>
2021-06-19 09:33:10 +0000

    Update CI

--- .github/setup.sh
+++ .github/setup.sh
@@ -3,12 +3,15 @@
 # Runs on GitHub actions to make some changes to allow the config check to work
 set -e
 
+# Set secrets file
 mv secrets_example.yaml secrets.yaml
 
+# Add fake ssl files
 mkdir ssl
 touch ssl/fullchain.pem 
 touch ssl/privkey.pem
 
+# Remove custom_components config files
 while read -r line; do
     if [ -f "integrations/${line}" ]; then
         rm "integrations/${line}"
